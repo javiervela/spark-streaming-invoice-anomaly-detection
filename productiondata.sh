f0f492965bd2657078dc988deed52dc5f91f9ff3
Javier Vela <javier.vela00@gmail.com>
2025-06-06 10:12:01 +0200

    Update production data script and add development dataset

--- productiondata.sh
+++ productiondata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 FILE=./src/main/resources/production.csv
+# FILE=./src/main/resources/development.csv
 java -classpath target/scala-2.11/anomalyDetection-assembly-1.0.jar es.dmr.uimp.simulation.InvoiceDataProducer ${FILE} purchases localhost:9092
